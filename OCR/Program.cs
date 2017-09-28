74c86c63833d939812aedb2769f6e248f5e8a8c4
Thomas Gossin <thomas.gossin@epita.fr>
2017-09-28 15:33:42 +0200

    recuperation image rgb

---
+++ OCR/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] t = newgrid();
+            t[2, 2] = 1;
+            t[2, 4] = 1;
+            t[2, 3] = 1;
+            t[3, 3] = 1;
+            t[4, 3] = 1;
+            t[5, 3] = 1;
+
+
+            int[,] v = newgrid();
+            v[5, 3] = 1;
+            v[5, 2] = 1;
+            v[4, 4] = 1;
+            v[4, 1] = 1;
+            v[3, 0] = 1;
+            v[3, 5] = 1;
+            Console.ReadKey();
+        }
+
+        static int[,] newgrid()
+        {
+            int[,] a = new int[6,6];
+       
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    a[i, j] = 0;
+                }
+            }
+            return a;
+        }
+        static void print(int[,] letter)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.Write(letter[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+    }
+}
---
+++ OCR/Vecteur.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OCR
+{
+    //Représente un vecteur de dimension N
+    public class Vecteur
+    {
+        //La liste des composantes du vecteur.
+        List<float> components;
+
+        //La dimension du vecteur, c'est-à-dire sa taille,
+        //ou, autrement dit, le nombre de composantes qu'il a.
+        public int Dimension
+        {
+            get { return components.Count; }
+        }
+
+        //Un indexeur permettant d'avoir accès aux composantes
+        //directement en utilisant la syntaxe a[i] et non pas
+        //a.components[i]
+        public float this[int index]
+        {
+            get { return components[index]; }
+        }
+
+        //Constructeur vide, ne fait que créer une liste de float vide.
+        public Vecteur()
+        {
+            this.components = new List<float>();
+        }
+
+        //Constructeur initialisant les composantes selon une liste déjà donnée.
+        //(Utilisé lors d'un chargement de donnée par le programme.)
+        public Vecteur(List<float> components)
+        {
+            this.components = components;
+        }
+
+        //Retourne une forme 'string' du vecteur.
+        //(Utilisée pour sauvegarder le vecteur.)
+        public override string ToString()
+        {
+            string str = components[0].ToString();
+            for (int i = 1; i < components.Count; i++)
+                str += "," + components[i];
+            return str;
+        }
+
+        //Ajoute une composante au vecteur.
+        //Ce design n'est sans doute pas le meilleur pour un vecteur,
+        //puisque, mathématicalement parlant, ils ont une taille fixe,
+        //mais du point de vue du programmeur, c'est le moyen le plus
+        //facile de faire les choses, le plus comfortable.
+        public void Add(float component)
+        {
+            this.components.Add(component);
+        }
+    }
+}

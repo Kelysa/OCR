adf0023e05388260401c38f2abbbdaf7d1d17722
julie siahou <julie.siahou@epita.fr>
2017-10-05 20:25:50 +0200

    noir et blanc + decoupage

--- Decoupage/main.c
+++ Decoupage/main.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <SDL/SDL_image.h>
+# include <SDL/SDL.h>
+# include <err.h>
+# include "pixel_operations.h"
 
 struct Coords
 {
@@ -14,121 +15,97 @@ struct Coords
 
 typedef struct Coordonnees Coordonnees;
 
-int main(int argc, char *argv[])
+int main(int argc, char *path[])
 {
-  //int tab[2][20] = {{0,5,0}, {0, 2},{0, 0}};
-    //printf("%d\n",tab[0][1]);
-    //int tab2[10] = {0,0,0,0};
-    //printf("%d\n", contains(tab));
-    //printf("taille de tab2 = %d\n",sizeof(tab2)/sizeof(int));
-    //printf("taille de tab = %d\n",sizeof(tab[0])/sizeof(int));
-    return 0;
+  decoupage(path);
+  return 0;
 }
 
-//int contains(int *tableau[]);
-//int contains(int *tableau[])
-//{
-//    int ligne = sizeof(tableau)/sizeof(int);
-//    int column = sizeof(tableau[0])/ligne;
-//    int i = 1;
-//    if (i ==  ligne)
-//    {
-//        return 10;
-//    }
-//    if (i ==  column)
-//    {
-//        return 20;
-//    }
-//    return 0;
-//}
-
-
-void decoupage(int *tableau[], int x, int y)
+void decoupage(char *path[])
 {
-    int i = 0;
-    int j = 0;
-    int beginposy =0;
-    int endposy =0;
-    int beginposx =0;
-    int endposx =0;
-    while (j<= y)
-    {
-        struct Coords coord;
-        for (j; j<y; j++)
-        {
-            for (i; i<y; i++)
-            {
-                if (tableau[i][j] == 0)
+   SDL_Surface *surface = load_image(path[1]);
+   Uint8 r = 0, g = 0, b = 0;
+   int j=0;
+   int i=0;
+   
+   while (j<= surface->h)
+   {
+     struct Coords coord;
+     for (j = j ; j < surface->h; j++)
+     {
+       for (i = i ; i < surface->w; i++)
+       {
+	 SDL_GetRGB(getpixel(surface, i, j), surface->format, &r, &g, &b);
+         if ((r & g & b) == 0  && r == g && g == b))
                 {
-                    tableau[i-1][j] = 71; //point vert à gauche
-		    coord.beginposy = i;
-                    break;
+		  putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255));
+		  coord.beginposy = i;
+                  break;
                 }
-            }
-	    if (tableau[i][j] == 71)
-	    {
-	      break;
-	    }
+        }
+        if (b == 255 && (r & g) == 0 && r == g)
+	{
+           break;
+	}
 	    
         }
-	if (j == y)
+        if( j < surface->h)
 	{
 	  return; 
 	}
 	
         // findendletters demarque la fin d'une lettre en cherchant une ligne blanche
         // colonne verte pour separer la fin d'une lettre
-        coord.endposy = findendletters(tableau,x,y,j);
-        for (i; i<y; i++)
+        coord.endposy = findendletters(surface,j);
+        for (i; i < surface->h; i++)
         {
-            for (j = beginposx; j<endposy; j++)
+            for (j = coord.beginposx; j<coord.endposy; j++)
             {
-                if (tableau[i][j] == 0)
+	      if ((r & g & b) == 0  && r == g && g == b))
                 {
-                    tableau[i-1][j] = 71;//point vert en haut
-                    coord.beginposx = i;
+                    putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255));//point vert en haut
+                    coord.beginposy = i;
                     break;
                 }
 	
             }
-	    if (tableau[i][j] == 71)
+	    if (b == 255 && (r & g) == 0 && r == g))
 	    {
 	      break;
 	    }
 
         }
-        for (i=x; i>0; i--)
+        for (i = surface->h ; i>0; i--)
         {
             for (j = beginposx; j<endposy; j++)
             {
-                if (tableau[i][j] == 0)
+                if ((r & g & b) == 0  && r == g && g == b))
                 {
                     coord.endposx = i;
-                    tableau[i+1][j] = 71; //point vert en bas
+                    putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255)); //point vert en bas
                     break;
                 }
 
             }
-	    if (tableau[i][j] == 71)
+	    if (b == 255 && (r & g) == 0 && r == g)
 	    {
 	      break;
 	    }
          }
 	// la il faut creer une tableau [][] aloue et mettre la struc coord dedant
     }
-
-
 }
 
-int findendletters(int *tableau[],int x ,int y, int posy)
+int findendletters(SDL_Surface *surface, int posy)
 {
     int n = 0;
     int posx =0;
-    while (n != y || posy<= y)
+    Uint8 r = 0, g = 0, b = 0;
+    while (n != surface->h || posy<= surface->h)
     {
-        if (tableau[posx][posy] == 255)
+        if ((r & g & b) == 255  && r == g && g == b))
             n+=1;
-        if (posx >= x)
+        if (posx >= surface->w)
         {
             posx=0;
             posy+=1;
@@ -143,4 +120,24 @@ int findendletters(int *tableau[],int x ,int y, int posy)
     //   posx -=1 ;
     //}
     return posy;
+}
+void drawLines(SDL_Surface *surface, int x, int y)
+{
+  SDL_LockSurface(surface);
+  for (int i = 0; i < surface -> w; i++)
+  {
+    putpixel(surface, i + x ,y-1, SDL_MapRGB(surface->format, 0, 0, 255));
+  }
+  SDL_UnlockSurface(surface);
+}
+void drawBorders(SDL_Surface *surface, int x, int y)
+{
+  SDL_LockSurface(surface);
+  
+  for (int j = y; j < surface -> h; j++)
+  {
+     putpixel(surface, x - 1, j, SDL_MapRGB(surface->format, 0, 0, 255));
+  }
+  
+  SDL_UnlockSurface(surface);
 }

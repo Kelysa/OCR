7a6cd74e2607e4cd929cf9762e08b4c65209ef55
victor chemla <victor.chemla@epita.fr>
2017-10-12 16:46:42 +0200

    add border line

--- Decoupage/main.c
+++ Decoupage/main.c
@@ -15,85 +15,20 @@ struct Coords
 
 typedef struct Coordonnees Coordonnees;
 
-int main(int argc, char *path[])
-{
-  decoupage(path);
-  return 0;
-}
-
-void decoupage(char *path[])
-{
-   SDL_Surface *surface = load_image(path[1]);
-   Uint8 r = 0, g = 0, b = 0;
-   int j=0;
-   int i=0;
-   
-   while (j<= surface->h)
-   {
-     struct Coords coord;
-     for (j = j ; j < surface->h; j++)
-     {
-       for (i = i ; i < surface->w; i++)
-       {
-	 SDL_GetRGB(getpixel(surface, i, j), surface->format, &r, &g, &b);
-         if ((r & g & b) == 0  && r == g && g == b))
-                {
-		  putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255));
-		  coord.beginposy = i;
-                  break;
-                }
-        }
-        if (b == 255 && (r & g) == 0 && r == g)
-	{
-           break;
-	}
-	    
-        }
-        if( j < surface->h)
-	{
-	  return; 
-	}
-	
-        // findendletters demarque la fin d'une lettre en cherchant une ligne blanche
-        // colonne verte pour separer la fin d'une lettre
-        coord.endposy = findendletters(surface,j);
-        for (i; i < surface->h; i++)
-        {
-            for (j = coord.beginposx; j<coord.endposy; j++)
-            {
-	      if ((r & g & b) == 0  && r == g && g == b))
-                {
-                    putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255));//point vert en haut
-                    coord.beginposy = i;
-                    break;
-                }
-	
-            }
-	    if (b == 255 && (r & g) == 0 && r == g))
-	    {
-	      break;
-	    }
-
-        }
-        for (i = surface->h ; i>0; i--)
-        {
-            for (j = beginposx; j<endposy; j++)
-            {
-                if ((r & g & b) == 0  && r == g && g == b))
-                {
-                    coord.endposx = i;
-                    putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255)); //point vert en bas
-                    break;
-                }
-
-            }
-	    if (b == 255 && (r & g) == 0 && r == g)
-	    {
-	      break;
-	    }
-         }
-	// la il faut creer une tableau [][] aloue et mettre la struc coord dedant
+void wait_for_keypressed(void) {
+  SDL_Event             event;
+  // Infinite loop, waiting for event
+  for (;;) {
+    // Take an event
+    SDL_PollEvent( &event );
+    // Switch on event type
+    switch (event.type) {
+    // Someone pressed a key -> leave the function
+    case SDL_KEYDOWN: return;
+    default: break;
     }
+  // Loop until we got the expected event
+  }
 }
 
 int findendletters(SDL_Surface *surface, int posy)
@@ -103,7 +38,7 @@ int findendletters(SDL_Surface *surface, int posy)
     Uint8 r = 0, g = 0, b = 0;
     while (n != surface->h || posy<= surface->h)
     {
-        if ((r & g & b) == 255  && r == g && g == b))
+        if ((r & g & b) == 255  && r == g && g == b)
             n+=1;
         if (posx >= surface->w)
         {
@@ -121,10 +56,113 @@ int findendletters(SDL_Surface *surface, int posy)
     //}
     return posy;
 }
+
+SDL_Surface* display_image(SDL_Surface *img) {
+  SDL_Surface          *screen;
+  // Set the window to the same size as the image
+  screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+  if ( screen == NULL ) {
+    // error management
+    errx(1, "Couldn't set %dx%d video mode: %s\n",
+         img->w, img->h, SDL_GetError());
+  }
+ 
+  /* Blit onto the screen surface */
+  if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+    warnx("BlitSurface error: %s\n", SDL_GetError());
+ 
+  // Update the screen
+  SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+ 
+  // wait for a key
+  wait_for_keypressed();
+ 
+  // return the screen for further uses
+  return screen;
+}
+void decoupage(char *path[])
+{
+   SDL_Surface *surface = IMG_Load(path[1]);
+   Uint8 r = 0, g = 0, b = 0;
+   int j=0;
+   int i=0;
+   
+   while (j<= surface->h)
+   {
+     struct Coords coord;
+     coord.beginposy=0;
+     coord.beginposx=0;
+     for (j = j ; j < surface->h; j++)
+     {
+       for (i = i ; i < surface->w; i++)
+       {
+	       SDL_GetRGB(getpixel(surface, i, j), surface->format, &r, &g, &b);
+         if ((r & g & b) == 0  && r == g && g == b)
+                {
+            		  putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255));
+		              coord.beginposy = i;
+                  break;
+                }
+        }
+        if (b == 255 && (r & g) == 0 && r == g)
+        	{
+           break;
+	         }
+	    
+        }
+        if( j < surface->h)
+	     {
+	       return; 
+	     }
+	     
+        // findendletters demarque la fin d'une lettre en cherchant une ligne blanche
+        // colonne verte pour separer la fin d'une lettre
+        coord.endposy = findendletters(surface,j);
+        for (; i < surface->h; i++)
+        {
+            for (j = coord.beginposy; j<coord.endposy; j++)
+            {
+	            if ((r & g & b) == 0  && r == g && g == b)
+                {
+                    putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255));//point vert en haut
+                    coord.beginposx = i;
+                    break;
+                }
+	
+            }
+	    if (b == 255 && (r & g) == 0 && r == g)
+	    {
+	      break;
+	    }
+    
+
+        }
+        for (i = surface->h ; i>0; i--)
+        {
+            for (j = coord.beginposx; j<coord.endposy; j++)
+            {
+                if ((r & g & b) == 0  && r == g && g == b)
+                {
+                    coord.endposx = i;
+                    putpixel(surface, i-1, j, SDL_MapRGB(surface->format,0, 0, 255)); //point vert en bas
+                    break;
+                }
+
+            }
+	    if (b == 255 && (r & g) == 0 && r == g)
+	    {
+	      break;
+	    }
+     }
+	// la il faut creer une tableau [][] aloue et mettre la struc coord dedant
+    }
+}
+
+
 void drawLines(SDL_Surface *surface, int x, int y)
 {
   SDL_LockSurface(surface);
-  for (int i = 0; i < surface -> w; i++)
+  for (int i = x; i < surface -> w; i++)
   {
     putpixel(surface, i + x ,y-1, SDL_MapRGB(surface->format, 0, 0, 255));
   }
@@ -140,4 +178,20 @@ void drawBorders(SDL_Surface *surface, int x, int y)
   }
   
   SDL_UnlockSurface(surface);
+}
+
+int main(int argc, char *path[])
+{
+  if (argc!=2)
+  {
+    return 1;
+  }
+  SDL_Surface *surface = IMG_Load(path[1]);
+  drawLines(surface,50,50);
+  drawBorders(surface,50,50);
+  display_image(surface);
+
+  SDL_FreeSurface(surface);
+  //decoupage(path);
+  return 0;
 }

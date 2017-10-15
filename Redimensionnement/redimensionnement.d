8e946230b42b950f54a0bf5135d48aba549a234d
Alexi Vandevoorde <alexi.vandevoorde@epita.fr>
2017-10-16 00:23:37 +0200

    Cleaning

--- Redimensionnement/redimensionnement.d
+++
@@ -1,3 +0,0 @@
-redimensionnement: redimensionnement.c /usr/include/SDL/SDL_image.h \
- /usr/include/SDL/SDL.h /usr/include/SDL/SDL_version.h \
- /usr/include/SDL/begin_code.h /usr/include/SDL/close_code.h

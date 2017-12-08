860cb8a3b6d347c71b941944f47079f32fbf0d43
Alexi VANDEVOORDE <alexi.vandevoorde@gmail.com>
2017-12-08 22:31:34 +0100

    Fix double pointers matrix

--- Preprocessing/resize.c
+++ Preprocessing/resize.c
@@ -7,6 +7,11 @@ struct matrix {
   double *tab;
 };
 
+struct matrix_d {
+  size_t h, w;
+  double **tab;
+};
+
 void print_matrix(double mat[], size_t lines, size_t cols)
 {  
   for(size_t i = 0; i < lines; i++) {
@@ -17,12 +22,69 @@ void print_matrix(double mat[], size_t lines, size_t cols)
   printf("\n");
 }
 
-void resize_matrix(struct matrix *src, struct matrix *dst)
+void print_matrix_d(double **mat, size_t lines, size_t cols)
 {
+  for(size_t i = 0; i < lines; i++) {
+    printf("\n");
+    for(size_t j = 0; j < cols; j++) {
+      printf("%.2f   ", mat[i][j]);
+    }
+  }
+  printf("\n");
+
+}
+
+struct matrix_d *from_matrix_to_matrix_d(struct matrix *mat)
+{
+  struct matrix_d *res = malloc(sizeof(struct matrix_d));
+  res->h = mat->h;
+  res->w = mat->w;
+  res->tab = malloc(sizeof(double)*res->h);
+  
+  for(size_t i = 0; i < mat->h; i++) {
+    res->tab[i] = malloc(sizeof(double)*res->w);
+    for(size_t j = 0; j < mat->w; j++) {
+      //res->tab[i][j] = malloc(sizeof(double));
+      res->tab[i][j] = mat->tab[j+i*mat->w];
+    }
+  }
+
+  free(mat->tab);
+  free(mat);
+  return res;
+}
+
+struct matrix *from_matrix_d_to_matrix(struct matrix_d *mat)
+{
+  struct matrix *res = malloc(sizeof(struct matrix));
+  res->h = mat->h;
+  res->w = mat->w;
+  res->tab = malloc(sizeof(double)*res->h*res->w);
+
+  for(size_t i = 0; i < mat->h; i++) {
+    for(size_t j = 0; j < mat->w; j++) {
+      res->tab[j+i*mat->w] = mat->tab[i][j];
+    }
+    free(mat->tab[i]);
+  }
+  
+  free(mat->tab);
+  free(mat);
+  return res;
+}
+
+struct matrix *resize_matrix(struct matrix *src, size_t newH, size_t newW)
+{
+
+  struct matrix *dst = malloc(sizeof(struct matrix));
+  dst->h = newH;
+  dst->w = newW;
+  dst->tab = malloc(sizeof(double)*dst->h*dst->w);
+  
   size_t coefH = floor((double)src->h/(double)dst->h);
   size_t coefW = floor((double)src->w/(double)dst->w);
 
-  printf("COEFW : %ld \n\n", coefW);
+  //printf("COEFW : %ld \n\n", coefW);
   
   double *temp = malloc(sizeof(double)*src->h*dst->w);
   
@@ -31,14 +93,14 @@ void resize_matrix(struct matrix *src, struct matrix *dst)
     for(size_t j = 0; j < src->w /*dst->w*/; j++) {
       buff += src->tab[i*src->w+j];
       if((j+1)%coefW == 0) {
-	printf("%ld & %d & %.2f\n", j, i*dst->w+j/coefW, buff/(double)coefW);
+	//printf("%ld & %d & %.2f\n", j, i*dst->w+j/coefW, buff/(double)coefW);
 	temp[i*dst->w+j/coefW] = /*(double)floor*/(buff/(double)coefW);
 	buff = 0;
       }
     }
   }
 
-  print_matrix(temp, src->h, dst->w);
+  //print_matrix(temp, src->h, dst->w);
 
   for(size_t i = 0; i < dst->w; i++) {
     double buff = 0;
@@ -51,7 +113,15 @@ void resize_matrix(struct matrix *src, struct matrix *dst)
       }
     }
   }
-  free(temp); 
+  free(temp);
+
+  return dst;
+}
+
+struct matrix_d *resize_matrix_d(struct matrix_d *src, size_t newH, size_t newW)
+{
+  struct matrix *temp = from_matrix_d_to_matrix(src);
+  return from_matrix_to_matrix_d(resize_matrix(temp, newH, newW));
 }
 
 int main()
@@ -67,18 +137,18 @@ int main()
   m->tab[10] = 1;
   m->tab[20] = 1;
   
-  struct matrix *m_resize = malloc(sizeof(struct matrix));
-  m_resize->h = 5;
-  m_resize->w = 5;
-  m_resize->tab = malloc(sizeof(double)*m_resize->h*m_resize->w);
-
-  resize_matrix(m, m_resize);
+  struct matrix *m_resize = resize_matrix(m, 5, 5);
   
   print_matrix(m->tab, m->h, m->w);
   print_matrix(m_resize->tab, m_resize->h, m_resize->w);
   
   free(m->tab);
   free(m);
-  free(m_resize->tab);
-  free(m_resize);
+  /*
+    free(m_resize->tab);
+    free(m_resize);
+  */
+
+  struct matrix *m_cpy = from_matrix_d_to_matrix(from_matrix_to_matrix_d(m_resize));
+  print_matrix(m_cpy->tab, m_cpy->h, m_cpy->w);
 }
